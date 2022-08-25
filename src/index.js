cba37eea5c7bf8ea6ee0351012074a85bbec9fdc
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-25 04:36:39 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const toyCollection = document.getElementById("toy-collection");
+let likeBtn,card;
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +14,87 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetchRequest();
 });
+
+function fetchRequest() {
+  fetch("http://localhost:3000/toys")
+    .then((resp) => resp.json())
+    .then((data) => {
+      addNewToy(data);
+    });
+}
+
+const appendImage = (toy) => {
+  let number = toy.likes;
+   card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+<h2>${toy.name}</h2>
+<img src=${toy.image} class="toy-avatar" />
+<p>${number} Likes</p>
+<button class="like-btn" id="${toy.id}">Like ❤️</button>
+`;
+  toyCollection.appendChild(card);
+  likeBtn = document.getElementById(`${toy.id}`);
+  likeBtn.addEventListener("click", function () {
+    updateCount(toy.id,number);
+  });
+};
+
+function addNewToy(toys) {
+  toys.map(appendImage);
+}
+
+const form = document.getElementsByClassName("add-toy-form")[0];
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let name = form[0].value;
+  let imageSrc = form[1].value;
+  console.log(name);
+  console.log(imageSrc);
+
+  createToy(name, imageSrc);
+});
+
+function createToy(name, url) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      image: `${url}`,
+      likes: 4,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      appendImage(data);
+    });
+}
+
+function updateCount(num,likesMade) {
+  card.querySelector("p").textContent = `${likesMade++} Likes`;
+  fetch(`http://localhost:3000/toys/${num}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify({
+      likes: likesMade,
+    }),
+  })
+
+  .then(resp=>resp.json())
+  .then((data)=>{
+console.log(data.likes)
+  })
+}
